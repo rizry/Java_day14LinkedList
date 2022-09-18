21af8dff3cc7f7a9cdf2d740e1f37e454f68f3fd
rizry <rizzwan.ansarii@gmail.com>
2022-09-18 13:09:50 +0530

    added nodes to the list

--- LinkedLists.java
+++ LinkedLists.java
@@ -3,6 +3,18 @@ package com.java.day14.llist;
 public class LinkedLists<E> {
 
   Node<E> head;
+
+  public void addLast(E item) {
+    Node<E> newNode = new Node<>(item);
+    if (head == null) head = newNode;//node being added is the first node
+    else if (head.next == null) head.next = newNode;  //node being added is the second node
+    else {
+      Node<E> temp = head;
+      while (temp.next != null) temp = temp.next; //traverse to the last node
+      temp.next = newNode;
+    }
+  }
+
 }
 
 class Node<E> {
--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ package com.java.day14.llist;
 public class Main {
 
   public static void main(String[] args) {
+
     LinkedLists<Integer> linkedL = new LinkedLists<>();
 
+    linkedL.addLast(56);
+    linkedL.addLast(30);
+    linkedL.addLast(70);
   }
 
 }
